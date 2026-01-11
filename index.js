19909e4edc0a61f3fd512bae6f20c25cacc8eee6
Aisha <husseinaisha256@gmail.com>
2026-01-11 20:14:10 +0300

    complete

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solution in this file!
+// Alternative with constants
+const HQ_STREET = 42;
+const FEET_PER_BLOCK = 264;
+
+function distanceFromHqInBlocks(street) {
+  return Math.abs(street - HQ_STREET);
+}
+
+function distanceFromHqInFeet(street) {
+  return distanceFromHqInBlocks(street) * FEET_PER_BLOCK;
+}
+
+function distanceTravelledInFeet(start, destination) {
+  return Math.abs(destination - start) * FEET_PER_BLOCK;
+}
+
+function calculatesFarePrice(start, destination) {
+  const distance = distanceTravelledInFeet(start, destination);
+  
+  if (distance <= 400) return 0;
+  if (distance <= 2000) return (distance - 400) * 0.02;
+  if (distance <= 2500) return 25;
+  return 'cannot travel that far';
+}
